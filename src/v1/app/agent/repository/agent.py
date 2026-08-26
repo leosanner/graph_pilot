732e5f38cd9a958406a8c32026913b71a75713fc
Leonardo Sanner <leosanner12@gmail.com>
2026-08-25 21:33:48 -0300

    .

---
+++ src/v1/app/agent/repository/agent.py
@@ -0,0 +1,3 @@
+from psycopg_pool import ConnectionPool
+from pydantic_settings import BaseSettings
+
---
+++ src/v1/infra/database.py
@@ -0,0 +1,25 @@
+from psycopg_pool import ConnectionPool
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class PostgresSettings(BaseSettings):
+  url: str
+  min_connections: int = 1
+  max_connections: int = 10
+
+  model_config = SettingsConfigDict(
+    env_prefix="DATABASE_"
+  )
+
+class PostgresClient:
+  def __init__(self, config: PostgresSettings):
+      self.pool = ConnectionPool(
+         conninfo = config.url,
+         min_size = config.min_connections,
+         max_size = config.max_connections
+      )
+
+  def connection(self):
+     return self.pool.connection()
+
+  def close(self):
+    self.pool.close()
