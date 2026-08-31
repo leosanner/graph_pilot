feeaa0f43100e2f742461b6874cf0a10bff87900
Leonardo Sanner <leosanner12@gmail.com>
2026-08-31 13:29:03 -0300

    .

--- src/v1/infra/log/log.py
+++ src/v1/infra/log/log.py
@@ -1,10 +1,24 @@
 import logging
 from pathlib import Path
-
+from dataclasses import dataclass, field
+from enum import IntEnum
 import structlog
 
 
-def configure_logging(path: Path) -> None:
+class LogLevel(IntEnum):
+    NOTSET = logging.NOTSET
+    DEBUG = logging.DEBUG
+    INFO = logging.INFO
+    WARNING = logging.WARNING
+
+@dataclass
+class LogConfig:
+    path: Path
+    log_level: LogLevel= field(default=logging.NOTSET)
+
+
+def configure_logging(config: LogConfig) -> None:
+    path = config.path
     path.parent.mkdir(parents=True, exist_ok=True)
 
     structlog.configure(
@@ -13,7 +27,7 @@ def configure_logging(path: Path) -> None:
             structlog.processors.TimeStamper(fmt="iso"),
             structlog.processors.JSONRenderer(),
         ],
-        wrapper_class=structlog.make_filtering_bound_logger(logging.INFO),
+        wrapper_class=structlog.make_filtering_bound_logger(config.log_level),
         logger_factory=structlog.WriteLoggerFactory(
             file=path.open("a", encoding="utf-8")
         ),
